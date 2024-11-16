1725372cadb49a48c35c18ef487c34b4a564b759
PengJiang <211123120051@zjut.edu.cn>
2024-11-16 16:18:05 +0800

    Initial commit

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,13 @@
+public class TwoSum{
+      public static void main(String[] args) {
+           Solution solution = new TwoSum().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return  null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
